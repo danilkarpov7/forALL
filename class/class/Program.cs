574ce806111dc713f2d72ab01f6254fbf944d319
danilkarpov7 <danilkarpov7@gmail.com>
2025-04-24 19:04:23 +0200

    clasmycar

---
+++ class/class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+Car myCar = new Car();
+myCar.Print();
+class Car
+{
+    public string brand = "Toyota";
+    public string model = "Camry";
+    public int year = 2020;
+
+
+    public void Print()
+    {
+        Console.WriteLine($"marka: {brand}");
+        Console.WriteLine($"model: {model}");
+        Console.WriteLine($"vipusk: {year}");
+    }
+
+}
---
+++ class/class/mycar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Car
+{
+    static void Main()
+    {
+        Car myCar = new Car();      // создаём объект
+        myCar.Print();          // вызываем метод для вывода информации
+
+        // Можно также изменить значения полей:
+        myCar.brand = "BMW";
+        myCar.model = "X5";
+        myCar.year = 2023;
+
+        Console.WriteLine("\nОбновлённая информация:");
+        myCar.Print();
+    }
+}
